80cc7e782612865de2cbccc7119709a907aa7682
Leonid Kibirev <leonid.kibirev@bk.ru>
2021-04-02 22:21:01 +0500

    Типы данных упр1

---
+++ less2/script.js
@@ -0,0 +1,36 @@
+let money = 100000;
+
+let income = '50000';
+
+let addExpress = '500 ,1000, 3000';
+
+let deposit = true;
+
+let misson = 500000;
+
+let period = 8;
+
+console.log(typeof money);
+
+console.log(typeof income);
+
+console.log(typeof deposit);
+
+console.log(addExpress.length);
+
+console.log('Период равен ' + period);
+
+console.log('Цель заработать ' + misson + ' рублей');
+
+addExpress.toLowerCase();
+
+addExpress = addExpress.split();
+
+console.log(addExpress);
+
+let budgetDay = money / 30;
+
+console.log(budgetDay);
+
+
+
